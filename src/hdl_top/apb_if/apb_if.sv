406b1efe5afdfbd1455ec756888c8ac0186e2d68
shweta.mbytes <shwetapatil@mirafra.com>
2021-11-02 00:38:22 +0530

    made changes in interface and added doc file

--- src/hdl_top/apb_if/apb_if.sv
+++ src/hdl_top/apb_if/apb_if.sv
@@ -6,29 +6,29 @@
 //  Declaration of pin level signals for apb interface
 //--------------------------------------------------------------------------------------------
 interface apb_if;
-  /*output reg  psel;
-  output reg  penable;    
-  output reg  pwrite;
-  output reg [7:0] paddr;
-  output reg [7:0] pwdata;                     	
-  input clk;   
-  input rst;
-  input [20:0] system_bus;      
-  input [7:0] prdata;
+  /*logic  psel;
+  logic  penable;    
+  logic  pwrite;
+  logic [7:0] paddr;
+  logic [7:0] pwdata;                     	
+  logic clk;   
+  logic rst;
+  logic [20:0] system_bus;      
+  logic [7:0] prdata;
   
   parameter IDLE = 2'd0, SETUP = 2'd1, ACCESS  = 2'd2;
   parameter READ= 2'd0, WRITE=2'd1;
   
-  reg [1:0] state; 
-  reg [7:0] pwdata1;
-  reg [7:0] paddr1;
-  reg penable1;
+  logic [1:0] state; 
+  logic [7:0] pwdata1;
+  logic [7:0] paddr1;
+  logic penable1;
 
-  reg [7:0] sys_data;
-  reg [7:0] sys_addr;
-  reg [1:0]  sys_kind;
-  reg [ 1 : 0 ]sel slave;
-  reg sys_active;*/ 
+  logic [7:0] sys_data;
+  logic [7:0] sys_addr;
+  logic [1:0]  sys_kind;
+  logic [ 1 : 0 ]sel slave;
+  logic sys_active;*/ 
 endinterface : apb_if
  
 `endif
