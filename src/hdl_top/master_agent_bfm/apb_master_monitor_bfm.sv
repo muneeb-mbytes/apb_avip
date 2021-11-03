d4f445b20c0864803207b29d4fd405bbb01b1f8d
Sahana P <91043932+sahanapalesh@users.noreply.github.com>
2021-11-03 16:59:35 +0530

    Update apb_master_monitor_bfm.sv
    Changed handle name for master monitor in hdl

--- src/hdl_top/master_agent_bfm/apb_master_monitor_bfm.sv
+++ src/hdl_top/master_agent_bfm/apb_master_monitor_bfm.sv
@@ -10,7 +10,7 @@ interface apb_master_monitor_bfm (apb_if intf);
   // Creating the handle for proxy driver
   //-------------------------------------------------------
   import apb_master_pkg::apb_master_monitor_proxy;
-  apb_master_monitor_proxy apb_master_mon_proxy;
+  apb_master_monitor_proxy apb_master_mon_proxy_h;
 
   initial begin
     $display("apb master Monitor BFM");
