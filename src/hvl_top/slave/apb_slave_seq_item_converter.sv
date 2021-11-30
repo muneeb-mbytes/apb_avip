515b623aef13b2eae33aba02cd4bf979d0ef7097
Shweta Patil <91044278+Shwetapatil05@users.noreply.github.com>
2021-11-30 15:26:04 +0530

    Update apb_slave_seq_item_converter.sv

--- src/hvl_top/slave/apb_slave_seq_item_converter.sv
+++ src/hvl_top/slave/apb_slave_seq_item_converter.sv
@@ -41,10 +41,8 @@ endfunction:new
 //--------------------------------------------------------------------------------------------
 function void apb_slave_seq_item_converter::from_class(input apb_slave_tx input_conv_h, 
                                                        output apb_transfer_char_s output_conv);
-  output_conv.paddr = input_conv_h.paddr;
   output_conv.pprot = input_conv_h.pprot;
   output_conv.psel = input_conv_h.psel;
-  output_conv.penable = input_conv_h.penable;
   output_conv.pwrite = input_conv_h.pwrite;
   output_conv.pwdata = input_conv_h.pwdata;
   output_conv.pstrobe = input_conv_h.pstrobe;
@@ -66,10 +64,8 @@ function void apb_slave_seq_item_converter::to_class(input apb_transfer_char_s i
                                                       output apb_slave_tx output_conv_h);
   output_conv_h = new();
 
-  output_conv_h.paddr = input_conv.paddr;
   output_conv_h.pprot = input_conv.pprot;
   output_conv_h.psel = input_conv.psel;
-  output_conv_h.penable = input_conv.penable;
   output_conv_h.pwrite = input_conv.pwrite;
   output_conv_h.pwdata = input_conv.pwdata;
   output_conv_h.pstrobe = input_conv.pstrobe;
@@ -87,11 +83,7 @@ endfunction: to_class
 function void apb_slave_seq_item_converter::do_print(uvm_printer printer);
   apb_transfer_char_s apb_st;
   super.do_print(printer);
-
-  foreach(apb_st.paddr[i])begin
-    printer.print_field($sformatf("paddr[%0d]",i),paddr[i],8,UVM_HEX);
-  end
-
+  
    foreach(apb_st.pwdata[i])begin
     printer.print_field($sformatf("pwdata[%0d]",i),pwdata[i],8,UVM_HEX);
   end
@@ -107,11 +99,7 @@ function void apb_slave_seq_item_converter::do_print(uvm_printer printer);
     foreach(apb_st.psel[i])begin
     printer.print_field("psel",apb_st.psel,2,UVM_BIN);
   end 
-
-  foreach(apb_st.penable[i])begin
-    printer.print_field("penable",apb_st.penable,2,UVM_BIN);
-  end 
-
+  
   foreach(apb_st.pwrite[i])begin
     printer.print_field("pwrite",apb_st.pwrite,2,UVM_BIN);
   end  
