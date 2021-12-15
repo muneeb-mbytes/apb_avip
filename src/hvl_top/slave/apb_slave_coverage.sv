cf85b58a1efc71560bb3a82b0425479f3256246a
maruthivangalli <maruthivangalli@mirafra.com>
2021-12-15 10:00:23 +0530

    added slave coverage

--- src/hvl_top/slave/apb_slave_coverage.sv
+++ src/hvl_top/slave/apb_slave_coverage.sv
@@ -1,3 +1,4 @@
+
 `ifndef APB_SLAVE_COVERAGE_INCLUDED_
 `define APB_SLAVE_COVERAGE_INCLUDED_
 
@@ -29,52 +30,60 @@ class apb_slave_coverage extends uvm_subscriber#(apb_slave_tx);
   //-------------------------------------------------------
   covergroup apb_slave_covergroup with function sample (apb_slave_agent_config cfg, apb_slave_tx packet);
   option.per_instance = 1;
-/*
-  //cheking the signal coverage
-  PWRITE_CP:coverpoint tx_cov.pwrite {
-    option.comment = "read and write conditon based on pwrite";
-    bins read = {0};
-    bins write = {1};
-  }
-  PENABLE_CP:coverpoint tx_cov.penable {
-    option.comment = "setup and access based on the enable";
-    bins setup  = {0};
-    bins access = {1};
-  }
 
-  PSELX_CP: coverpoint tx_cov.pselx {
+  //cheking the signal coverage
+  PWRITE_CP:coverpoint packet.pwrite{
+    option.comment = "read and write conditon based on pwrite";
+    bins mode = {[0:1]};
+    //bins write = {1};
+  }
+//  PENABLE_CP:coverpoint packet.penable {
+//    option.comment = "setup and access based on the enable";
+//    bins setup  = {0};
+//    bins access = {1};
+//  }
+
+  PSELX_CP: coverpoint packet.psel {
     option.comment = "no.of slaves used ";
     bins NO_OF_SLAVES[] = {[15:0]};
   }
 
-  PADDR_CP : coverpoint tx_cov.paddr {
+  PADDR_CP : coverpoint cfg.paddr {
     option.comment = "address range";
     bins addr = {[31:8]};
   }
-*/
-  PWDATA_CP: coverpoint tx_cov.pwdata {
+
+  PWDATA_CP: coverpoint packet.pwdata {
     option.comment = "write data range";
-    bins wdata = {[31:8]};
+    bins wdata_8bit = {8};
+    bins wdata_16bit = {16};
+    bins wdata_24bit = {24};
+    bins wdata_32bit = {32};
+
   }
-  PRDATA_CP : coverpoint tx_cov.prdata {
+  PRDATA_CP : coverpoint packet.prdata {
     option.comment = "read data range ";  
-    bins pread = {[31:8]};
+    bins wdata_8bit = {8};
+    bins wdata_16bit = {16};
+    bins wdata_24bit = {24};
+    bins wdata_32bit = {32};
+
   }
 
-  PSLVERR_CP:coverpoint tx_cov.pslverr {
+  PSLVERR_CP:coverpoint packet.pslverr {
     option.comment = "error signal at the end of transfer";
     bins err = {1};
     bins ok = {0};
   }
 
- //  PSTRB_CP :  coverpoint tx_cov.pstrb {
+ //  PSTRB_CP :  coverpoint packet.pstrb {
  //   option.comment = "error signal at the end of transfer";
- // bins strb = {[3:0]};
+ //   bins strb = {[3:0]};
  // }
  //cross coverage 
-  //PADDR_X_PWDATA_: cross PADDR_CP,PWDATA_CP;
-  //PSEL_X_PENABLE_: cross PSEL_CP,PENABLE_CP;
-  //PADDR_X_PRDATA_: cross PADDR_CP,PRDATA_CP;
+  PADDR_X_PWDATA_ : cross PADDR_CP,PWDATA_CP;
+ // PSEL_X_PENABLE_ : cross PSEL_CP,PENABLE_CP;
+  PADDR_X_PRDATA_ : cross PADDR_CP,PRDATA_CP;
 
 endgroup : apb_slave_covergroup
 //-------------------------------------------------------
@@ -82,7 +91,7 @@ endgroup : apb_slave_covergroup
 //-------------------------------------------------------
  extern function new(string name = "apb_slave_coverage", uvm_component parent = null);
  extern virtual function void write(apb_slave_tx t);
-
+ extern virtual function void report_phase(uvm_phase phase);
 endclass : apb_slave_coverage
 
 //--------------------------------------------------------------------------------------------
@@ -94,6 +103,7 @@ endclass : apb_slave_coverage
 function apb_slave_coverage::new(string name = "apb_slave_coverage",uvm_component parent = null);
   super.new(name, parent);
   apb_slave_analysis_export = new("apb_slave_analysis_export",this);
+ // apb_slave_cover_group = new();
 endfunction : new
 
 //-------------------------------------------------------
@@ -103,6 +113,22 @@ endfunction : new
 //-------------------------------------------------------
 function void apb_slave_coverage::write(apb_slave_tx t);
   `uvm_info(get_type_name(),"APB SLAVE COVERAGE",UVM_LOW);
+  apb_slave_covergroup.sample(apb_slave_agent_cfg_h,t);
 endfunction: write
 
+//--------------------------------------------------------------------------------------------
+// Function: report_phase
+// Used for reporting the coverage instance percentage values
+//--------------------------------------------------------------------------------------------
+function void apb_slave_coverage::report_phase(uvm_phase phase);
+  `uvm_info(get_type_name(), $sformatf("Slave Agent Coverage = %0.2f %%",
+                                       apb_slave_covergroup.get_coverage()), UVM_NONE);
+endfunction: report_phase
 `endif
+
+
+
+
+
+    
+
