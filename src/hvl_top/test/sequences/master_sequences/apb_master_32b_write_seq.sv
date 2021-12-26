b1d0610620b8e11cdc585f6b5de2e5c61db46d4b
sivareddy_mbytes <sivareddykostam@mirafra.com>
2021-12-27 00:08:38 +0530

    32b_testcase up and running

--- src/hvl_top/test/sequences/master_sequences/apb_master_32b_write_seq.sv
+++ src/hvl_top/test/sequences/master_sequences/apb_master_32b_write_seq.sv
@@ -1,27 +1,27 @@
-`ifndef APB_MASTER_8B_READ_SEQ_INCLUDED_
-`define APB_MASTER_8B_READ_SEQ_INCLUDED_
+`ifndef APB_MASTER_32B_WRITE_SEQ_INCLUDE_
+`define APB_MASTER_32B_WRITE_SEQ_INCLUDE_
 
 //--------------------------------------------------------------------------------------------
-// Class: apb_master_8b_read_seq
+// Class: apb_master_32b_write_seq
 // Extends the apb_master_base_seq and randomises the req item
 //--------------------------------------------------------------------------------------------
-class apb_master_8b_read_seq extends apb_master_base_seq;
-  `uvm_object_utils(apb_master_8b_read_seq)
+class apb_master_32b_write_seq extends apb_master_base_seq;
+  `uvm_object_utils(apb_master_32b_write_seq)
 
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
-  extern function new(string name ="apb_master_8b_read_seq");
+  extern function new(string name ="apb_master_32b_write_seq");
   extern task body();
-  endclass : apb_master_8b_read_seq
+  endclass : apb_master_32b_write_seq
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
 //
 // Parameters:
-//  name - apb_master_8b_read_seq
+//  name - apb_master_32b_write_seq
 //--------------------------------------------------------------------------------------------
-function apb_master_8b_read_seq::new(string name="apb_master_8b_read_seq");
+function apb_master_32b_write_seq::new(string name="apb_master_32b_write_seq");
   super.new(name);
 endfunction : new
 
@@ -29,22 +29,20 @@ endfunction : new
 // Task : body
 // Creates the req of type master transaction and randomises the req.
 //--------------------------------------------------------------------------------------------
-task apb_master_8b_read_seq::body();
+task apb_master_32b_write_seq::body();
   super.body();
   req=apb_master_tx::type_id::create("req");
   req.apb_master_agent_cfg_h = p_sequencer.apb_master_agent_cfg_h;
   start_item(req);
   if(!req.randomize() with {req.pselx == SLAVE_0;
-                            req.paddr == 'h3;
-                            req.transfer_size == BIT_8;
-                            req.pwrite == READ;}) begin
+                            //req.paddr == 'h3;
+                            req.transfer_size == BIT_32;
+                            req.pwrite == WRITE;}) begin
     `uvm_fatal("APB","Rand failed");
   end
   //req.print();
   finish_item(req);
- 
-endtask : body
 
-
+endtask: body
 
 `endif
